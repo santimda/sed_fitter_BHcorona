627549c5db35be8cb7ee299e198e5ceded7705dd
santimda <santiagodp1990@gmail.com>
2025-06-27 11:33:46 +0200

    Added a filtering of data by beam size in the example

--- example.py
+++ example.py
@@ -8,16 +8,20 @@ import numpy as np
 # make sure the package is installed (from the project root):
 #    pip install -e .
 
-from smbh_corona.galaxy import Galaxy
+from smbh_corona.galaxy import Galaxy, Omega_beam
+from smbh_corona.constants import *
 
 def main():
     # ------------------------------------------------------------------------------
     # 1) Create the Galaxy object. It will
     #      • load parameters from data/parameters/NGC985.par
     #      • load flux data from data/fluxes/NGC985_flux.dat
+    #      • filter very-high-resolution or low resolution data (e.g, between 0.09"--1")
     # ------------------------------------------------------------------------------
     gal = Galaxy("NGC985")
-
+    Omega_beam_min, Omega_beam_max = Omega_beam(0.09*arcsec, 0.09*arcsec), Omega_beam(0.3*arcsec, 0.3*arcsec)
+    gal.filter_data_by_beam(Omega_beam_min=Omega_beam_min, Omega_beam_max=Omega_beam_max)
+    
     # ------------------------------------------------------------------------------
     # 2) Pick a grid of observing frequencies [Hz]
     # ------------------------------------------------------------------------------
