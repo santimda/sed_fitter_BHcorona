7523465cdad7051c56d3053ad0c713615fb4061f
santimda <santiagodp1990@gmail.com>
2025-06-27 10:56:52 +0200

    Fixed an error in the test (units are cgs)

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -1,13 +1,15 @@
 import pytest
 from smbh_corona import constants
+'''Check that the constants are loaded correctly'''
 
 def test_speed_of_light_positive():
     assert constants.c > 0
 
-def test_electron_mass_units():
-    # m_e should be ~9.11e-31 kg
-    assert pytest.approx(constants.me, rel=1e-3) == 9.109e-31
+def test_electron_mass_cgs_value():
+    # m_e should be ~9.11e-28 g
+    assert pytest.approx(constants.me/9.11e-28, rel=1e-2) == 1.0
 
 def test_sigma_T_value():
-    # Thomson cross-section ~6.65e-29 m²
-    assert pytest.approx(constants.sigma_T, rel=1e-3) == 6.652e-29
+    # Thomson cross-section ~6.65e-25 cm²
+    assert pytest.approx(constants.sigma_T/6.65e-25, rel=1e-3) == 1.0
+
