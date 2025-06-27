cf90cffd33014ba2505783e9bc3213fe5d690945
santimda <santiagodp1990@gmail.com>
2025-06-27 16:02:50 +0200

    Added ipynb to reproduce the plots of the parameter space exploration

--- src/smbh_corona/corona_sed.py
+++ src/smbh_corona/corona_sed.py
@@ -100,3 +100,25 @@ def calc_Pth_factor(r_c=80, T=1e9):
     theta_e = (k_B * T) / mec2   # Normalized temperature
     T_i = 9.1e10 * (20 / r_c)    # Ion temperature (Eq. 1 in Inoue+2024)
     return (1 + T_i / T) / a_fun(theta_e)
+
+
+def calc_gmax_cool(eta_g, r_c, B):
+    '''Calculate the maximum Lorentz factor of the electrons given by t_acc = t_syn.
+    The formulae are taken from Inoue+2019 (https://iopscience.iop.org/article/10.3847/1538-4357/ab2715/pdf)
+    eta_g: gyrofactor (mean free path in units of R_g)
+    r_c: coronal radius in units of R_s
+    B: magnetic field intensity'''
+    # t_sy = 7.7e4 * (B / 10 G)**-2 * (gamma/100)**-1 s
+    # t_acc = 7.6e-3 * (eta_g / 100) * (r_c/40) * (B / 10 G)**-1 * (gamma/100)**-1 s ! * (eta_acc/10)
+    factor = (7.7e4/7.6e-3) * (100/eta_g) * (40/r_c) * (10/B)
+    gmax_cool = 100 * np.sqrt(factor)
+    return gmax_cool
+
+
+def calc_gmax_conf(R, B):
+    '''Calculate the maximum Lorentz factor of the electrons given by confinement (Hillas criterium).
+    The formulae are taken from Inoue+2019 (https://iopscience.iop.org/article/10.3847/1538-4357/ab2715/pdf)
+    R: accelerator size
+    B: magnetic field intensity'''
+    gmax_conf = q_e * B * R / mec2
+    return gmax_conf
